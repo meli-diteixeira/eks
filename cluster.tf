fbc22e5611917225ff2814a7f51b1386e3de2d9e
Dihogo Teixeira <dihogo.teixeira@mercadolivre.com>
2022-06-14 12:27:29 -0300

    Update cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -7,13 +7,14 @@
 resource "aws_eks_cluster" "cc-eks-cluster" {
   name                      = var.cluster_name
   role_arn                  = var.role_eks_cluster_control_plane
-  enabled_cluster_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+  enabled_cluster_log_types = var.enabled_cluster_log_types
 
   vpc_config {
     security_group_ids      = [var.sg_cluster_control_plane]
     subnet_ids              = var.private_subnets
-    endpoint_private_access = true
-    endpoint_public_access  = true
+    endpoint_private_access = var.endpoint_private_access
+    endpoint_public_access  = var.endpoint_public_access
+    public_access_cidrs     = var.public_access_cidrs
   }
 }
 
